372d366c09f6924f9994ca52ee792067870b4e8d
Rafael Assis Fontes <fael_assis@hotmail.com>
2022-03-29 11:27:55 -0300

    aprendendo modulo em java

---
+++ funcoes.mjs
@@ -0,0 +1,17 @@
+function mostrarIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`
+}
+
+function mostrarCidade(nome, cidade){
+    return `A cidade de ${nome} é ${cidade}`
+}
+
+function mostrarHobby(nome, Hobby){
+    return `O hobby de ${nome} é ${Hobby}`
+}
+
+export{
+    mostrarIdade,
+    mostrarHobby,
+    mostrarCidade
+}
---
+++ main.mjs
@@ -0,0 +1,9 @@
+import{
+    mostrarCidade,
+    mostrarHobby,
+    mostrarIdade
+} from "./funcoes.mjs"
+
+console.log(mostrarIdade("Rafael",34))
+console.log(mostrarCidade("Rafael","Volta Redonda"))
+console.log(mostrarHobby("Rafael","Jogos"))
